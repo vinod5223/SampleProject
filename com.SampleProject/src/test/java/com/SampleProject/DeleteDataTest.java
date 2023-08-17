f9dc4753732925d63f8828ea83a1e9531a3ff7fc
USER <USER@DESKTOP-ORKTBJG>
2023-08-17 15:13:54 +0530

    sth

---
+++ com.SampleProject/src/test/java/com/SampleProject/DeleteDataTest.java
@@ -0,0 +1,11 @@
+package com.SampleProject;
+
+import org.testng.annotations.Test;
+
+public class DeleteDataTest {
+
+	@Test
+	public void deleteData() {
+		System.out.println("data has been deleted");
+	}
+}
